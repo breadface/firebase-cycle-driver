f4b9dff84d26dc065c1e843eec51ac4f48643d62
Omonori Jesmine <damilolao328@gmail.com>
2017-09-25 12:54:00 +0100

    read values from auth and database firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,41 +1,46 @@
 import xstream from 'xstream';
-import {unique, flatten} from 'lodash'
-// import firebase from 'firebase/app'
+import {unique, flatten} from 'lodash';
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
 
-const makeFirebaseDriver = baseRef => {
+const makeFirebaseDriver = baseConfig => {
+  firebase.initializeApp(baseConfig);
+  const db = firebase.database()
+  const auth = firebase.auth()
+
   return firebase_state$ => {
 
     return {
       select: (key) => {
         switch (key) {
           case "database":
-
             return (path) => xstream.create({
               start: (listener) => {
-                baseRef.database().ref.on('value', snapshot => {
+                db.ref(path).on('value', snapshot => {
                   listener.next(snapshot.val())
                 })
               },
               stop: () => {
-
+                //TODO -  unsubscribe from firebase database
               }
             })
 
           case "auth":
             return xstream.create({
               start: (listener) => {
-                baseRef.auth().onAuthStateChanged(user => {
+                auth.onAuthStateChanged(user => {
                   listener.next(user)
                 })
               },
               stop: () => {
                 // TODO Unsubscribe from firebase authentication
               },
-      })
+            })
 
-        default:
+          default:
 
-      }
+        }
       }
     }
   }
